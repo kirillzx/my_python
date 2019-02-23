ee6b986e0a20cef89be445745b536d25d23cb6c4
Kirill <47603462+kirillzx@users.noreply.github.com>
2019-02-23 18:06:17 +0300

    Delete if_random_game.py

--- HW_3/if_random_game.py
+++
@@ -1,9 +0,0 @@
-from random import *
-n = randint(0, 5)
-a = int(input('Guess the number: '))
-if a == n:
-    print('Victory!!! You guessed the number')
-elif a > n:
-    print('Its more than necessary\nTry again')
-elif a < n:
-    print('Its less than necessary\nTry again')
