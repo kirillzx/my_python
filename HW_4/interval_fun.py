54ae54e9249eea3a87295e61938e17655d93975b
Kirill <47603462+kirillzx@users.noreply.github.com>
2019-03-11 19:28:11 +0300

    HW_4

---
+++ HW_4/interval_fun.py
@@ -0,0 +1,4 @@
+y=0
+for i in range(10,30,2):
+    y=i**2+3
+print(y)    
---
+++ HW_4/new_list.py
@@ -0,0 +1,14 @@
+#using the for loop
+from math import sqrt
+a = [2,9,16,25]
+b = []
+for i in range(len(a)):
+    b.append(sqrt(a[i]))
+print(b)    
+
+#based on the map function
+print(list(map(sqrt,a)))
+
+#by the generator list
+c = [sqrt((a[i])) for i in range(len(a))]
+print(c)
